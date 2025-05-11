fd6b16c34d799145103360c7a9ad984780b92a43
lvainio <leojvainio@gmail.com>
2025-05-11 08:58:29 +0200

    url: add libcurl support and implement request function for HTTP requests.

--- src/url.h
+++ src/url.h
@@ -7,9 +7,14 @@ class Url {
 public:
     explicit Url(const std::string& rawUrl);
 
+    void request();
+
+    std::string toString();
+
 private:
     void parse(const std::string& rawUrl);
 
+    // FIXME: how do we name private members?
     std::string url_;
     std::string scheme_;
     std::string host_;
