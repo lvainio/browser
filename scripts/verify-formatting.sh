eb1ecdc8db189c41dbbc0dfd3ca4908f659f64a6
lvainio <leojvainio@gmail.com>
2025-05-16 09:50:16 +0200

    CI: Make sure format script runs with config file

--- scripts/verify-formatting.sh
+++ scripts/verify-formatting.sh
@@ -9,7 +9,7 @@ files=$(find "${dirs[@]}" -type f \( -name '*.cpp' -o -name '*.h' \))
 
 format_issues_found=0
 for file in $files; do
-  if ! diff -q <(clang-format "$file") "$file" >/dev/null; then
+  if ! diff -q <(clang-format -style=file "$file") "$file" >/dev/null; then
     echo "Formatting issue found in: $file"
     format_issues_found=1
   fi
