cb349b79d10a74b34ee00aa93f05ca7070f3f822
lvainio <leojvainio@gmail.com>
2025-05-17 12:59:38 +0200

    Test: Add test directory to format scripts

--- scripts/format-all.sh
+++ scripts/format-all.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
-dirs=($REPO_ROOT/src)
+dirs=($REPO_ROOT/src $REPO_ROOT/tests)
 
 for file in $(find "${dirs[@]}" -type f \( -name '*.cpp' -o -name '*.h' \)); do
   clang-format -i "$file"
