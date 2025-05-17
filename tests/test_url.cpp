7fa9bfd7948f561a1f55314de3f96e62153e49d3
lvainio <leojvainio@gmail.com>
2025-05-17 13:01:39 +0200

    Style: Format test file

--- tests/test_url.cpp
+++ tests/test_url.cpp
@@ -1,6 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
 #include "url.h"
+#include <doctest/doctest.h>
 
 TEST_CASE("FIXME: This should be a real test") {
     CHECK(false);
