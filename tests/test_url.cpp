ee6005db05eca4e68d92ed48a48a015e149c4a85
lvainio <leojvainio@gmail.com>
2025-05-17 12:57:59 +0200

    Test: add basic support for doctest
    Closes #26

---
+++ tests/test_url.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+#include "url.h"
+
+TEST_CASE("FIXME: This should be a real test") {
+    CHECK(false);
+    CHECK(true);
+}
